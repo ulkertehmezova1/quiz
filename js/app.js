9ff23cdbb6ab29ed85ba0dfa7c0436d6bbe58126
Ulker <ulkrthmzova@gmail.com>
2022-05-11 14:25:36 +0400

    New updates

---
+++ js/app.js
@@ -0,0 +1,53 @@
+const correctAnswers=['B','B','B','B'];
+const form=document.querySelector('.quiz-form');
+const result=document.querySelector('.result');
+form.addEventListener('submit',e=>{
+
+    e.preventDefault();
+    let score=0;
+
+    const userAnswer=[form.q1.value,form.q2.value,form.q3.value,form.q4.value];
+     userAnswer.forEach((answer,index) => {
+         
+        if (answer===correctAnswers[index]) {
+            
+            score+=25;
+        }
+     });
+
+scrollTo(0,0);
+
+result.classList.remove('d-none');
+
+
+
+let output=0;
+const timer=setInterval(() => {
+    result.querySelector('span').textContent=`${output}%`
+
+if (output===score) {
+    clearInterval(timer);
+}
+else{
+    output++;
+}
+}, 10);
+
+})
+
+
+
+
+// setTimeout(()=>{
+
+//     alert('Salam');
+// },4000);
+
+// let i=0;
+// const timer=setInterval(() => {
+//     console.log('salam');
+//     i++;
+//     if(i===5){
+//         clearInterval(timer);
+//     }
+// }, 1000);
